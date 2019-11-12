7b1bc731b4bfd71ec5d041df03fe6ede161ca991
arjunah <arjunah@abv.bg>
2019-11-12 18:26:49 +0200

    routing lecture

--- 03. Components - deep/03. React-JS-Components-Resources/origami/src/listItem/ListItem.js
+++ 03. Components - deep/03. React-JS-Components-Resources/origami/src/listItem/ListItem.js
@@ -3,7 +3,7 @@ import "./ListItem.css";
 import Logo from "../resources/white-origami-bird.png";
 
 function ListItem (props) {
-    const nums = [1,2,3,4,5,6,7,8,9,10,11];
+    const nums = [1,2,3,4,5,6,7,8,9];
     const items = nums.map(num => {
         return <li key={num} className="listItem"><a href="#">Going to {num}</a></li>
     });
